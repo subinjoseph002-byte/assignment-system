fb22313aa6127b229d84b1bcfa649e35390128b3
subinjoseph002-byte <subinjoseph002@gmail.com>
2026-05-07 01:47:38 +0530

    Force Django to serve uploads in production

--- assignment_system/urls.py
+++ assignment_system/urls.py
@@ -1,12 +1,10 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
-from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
+    re_path(r'^uploads/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
-
-# Serve media files in both development and production (for Render testing)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
